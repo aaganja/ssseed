b2799914941c06dfb218446ecbe1724f27f56b68
Kuber Aaganja <kuberaaganja@gmail.com>
2019-07-27 21:28:30 +0545

    added to top link when scroll the page

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,37 +1,46 @@
 $(document).ready(function() {
+  window.onscroll = function() {scrollFunction()};
 
-    function sssDynamicHeight() {
-        var avatarwidth = $('.welcome__inner--avatar').innerWidth();
-        var avatarheight = Math.round(avatarwidth * 9 / 16);
-        $('.sss_js_avatar').css("height", avatarheight + 'px');
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("toTop").style.display = "block";
+    } else {
+      document.getElementById("toTop").style.display = "none";
+    }
+  }
 
-        var ourCropswidth = $('.ourCrops__wrapper').innerWidth();
-        var ourCropsheight = Math.round(ourCropswidth * 4 / 3);
-        $('.ourCrops__content').css("height", ourCropsheight + 'px');
+  function sssDynamicHeight() {
+      var avatarwidth = $('.welcome__inner--avatar').innerWidth();
+      var avatarheight = Math.round(avatarwidth * 9 / 16);
+      $('.sss_js_avatar').css("height", avatarheight + 'px');
 
-        var ourGallerywidth = $('.gallery__container--item').innerWidth();
-        var ourGalleryheight = Math.round(ourGallerywidth * 3 / 4);
-        $('.gallery__container--item').css("height", ourGalleryheight + 'px');
+      var ourCropswidth = $('.ourCrops__wrapper').innerWidth();
+      var ourCropsheight = Math.round(ourCropswidth * 4 / 3);
+      $('.ourCrops__content').css("height", ourCropsheight + 'px');
 
-        // $('#sss_videoModal').on('load', function() {
-        var ourVideowidth = $('.modal-dialog').innerWidth();
-        var ourVideoheight = Math.round(ourVideowidth * 9 / 16);
-        $('.sss-videoContent').css("height", ourVideoheight + 'px');
-        // });
+      var ourGallerywidth = $('.gallery__container--item').innerWidth();
+      var ourGalleryheight = Math.round(ourGallerywidth * 3 / 4);
+      $('.gallery__container--item').css("height", ourGalleryheight + 'px');
 
-    };
-    var winWidth = $(window).innerWidth()
-    sssDynamicHeight()
-    $(window).resize(function() {
-        var winWidth = $(window).innerWidth()
-        sssDynamicHeight()
-    });
+      // $('#sss_videoModal').on('load', function() {
+      var ourVideowidth = $('.modal-dialog').innerWidth();
+      var ourVideoheight = Math.round(ourVideowidth * 9 / 16);
+      $('.sss-videoContent').css("height", ourVideoheight + 'px');
+      // });
 
-    $(".sss-hamburger").click(function() {
-        $('.sss-nav__menu').toggleClass('open');
-        $(this).toggleClass('ham_open')
-    });
-    $("#sss_gallery").lightGallery();
+  };
+  var winWidth = $(window).innerWidth()
+  sssDynamicHeight()
+  $(window).resize(function() {
+      var winWidth = $(window).innerWidth()
+      sssDynamicHeight()
+  });
+
+  $(".sss-hamburger").click(function() {
+      $('.sss-nav__menu').toggleClass('open');
+      $(this).toggleClass('ham_open')
+  });
+  $("#sss_gallery").lightGallery();
 
 });
 
